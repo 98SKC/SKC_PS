106acb2f0ec32c6212dcb53815802b380b7ebe80
98SKC <127596280+98SKC@users.noreply.github.com>
2023-11-21 20:08:18 +0900

    Update 뒤에 있는 큰 수 찾기.java

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -9,7 +9,7 @@ class Solution {
          
         for(int i=0 ; i<numbers.length ; i++) {
         	
-			if(stack.isEmpty() || numbers[i]<numbers[i-1]) {//스택이 비었거나 다음 숫자가 더 작으면 스택에 인덱스를 쌋는다.
+			if(stack.isEmpty() || numbers[i]<numbers[i-1]) {//스택이 비었거나 다음 숫자가 더 작으면 스택에 인덱스를 쌓는다.
 				stack.push(i);
 			} else {
 				while(!stack.isEmpty() && numbers[stack.peek()]<numbers[i]) {// 스택이 비어있지 않고, 스택의 top이 쌓으려는 숫자보다 작으면 해당 인덱스의 answer은 쌓으려는 값이다.
