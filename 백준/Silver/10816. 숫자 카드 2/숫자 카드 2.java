9fef7871275824ee8edcce161131cc4292022f53
98SKC <127596280+98SKC@users.noreply.github.com>
2023-12-05 13:54:27 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1184 ms, Memory: 177272 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,36 +1,38 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
-			int input = Integer.parseInt(st.nextToken());
-            
-			if(hash.get(input) == null) hash.put(input, 1);
-			else hash.put(input, hash.get(input) + 1);
-		}
-		
-		
-		int M = Integer.parseInt(br.readLine());
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < M; i++) {
-			int input = Integer.parseInt(st.nextToken());
-            
-			if(hash.get(input) == null) sb.append(0).append(" ");
-			else sb.append(hash.get(input)).append(" ");
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int N = Integer.parseInt(br.readLine());
+
+        int sub;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            sub = Integer.parseInt(st.nextToken());
+            map.put(sub, map.getOrDefault(sub, 0) + 1);
+        }
+        int M = Integer.parseInt(br.readLine());
+
+        int number;
+
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int j = 0; j < M; j++){
+            number = Integer.parseInt(st2.nextToken());
+            if(map.containsKey(number)){
+                sb.append(map.get(number)).append(" ");
+            }else{
+                sb.append(0).append(" ");
+            }
+        }
+        System.out.println(sb);
+    }
 }
