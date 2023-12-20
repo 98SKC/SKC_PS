82f5e8b4add833ab4f67f89ee5d18c8749f380ce
98SKC <127596280+98SKC@users.noreply.github.com>
2023-12-21 00:43:42 +0900

    [Bronze I] Title: 약수, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -10,19 +10,20 @@ public class Main{
     	//둘째 줄에는 N의 진짜 약수들.
     	//제일 먼저 떠오르는 건 정렬시키고, 첫번째랑 마지막을 곱하는 것.
     	//찾아보니 정렬을 안해도, 최대 최소를 꺼낼 수 있음. 배열 넣는 과정에서 진행
-    	
+    	//근데 이렇게 해보니, 배열을 굳이 만들어야하나?
     	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     	
     	int N=Integer.parseInt(br.readLine());
-    	int[] arr=new int[N];
+    	//int[] arr=new int[N];
+    	int sub=0;
     	StringTokenizer st=new StringTokenizer(br.readLine());
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
     	
     	for(int i=0;i<N;i++) {
-    		arr[i]=Integer.parseInt(st.nextToken());
-    		min=Math.min(min, arr[i]);
-    		max=Math.max(max, arr[i]);
+    		sub=Integer.parseInt(st.nextToken());
+    		min=Math.min(min, sub);
+    		max=Math.max(max, sub);
     	}
     	
 //    	Arrays.sort(arr);
