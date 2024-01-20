a210b9da12de6db4671e2bf7f55dad5d8610a975
98SKC <127596280+98SKC@users.noreply.github.com>
2024-01-20 23:24:32 +0900

    [Bronze II] Title: 나누기, Time: 140 ms, Memory: 15816 KB -BaekjoonHub

---
+++ 백준/Bronze/1075. 나누기/나누기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
+        
+        
+        n /= 100;
+        n *= 100;
+        int f = Integer.parseInt(br.readLine());
+        
+
+        while (n % f != 0) {
+            n++;
+            answer++;
+        }
+        if (answer < 10) {
+            System.out.print("0" + answer);
+        } else {
+            System.out.print(answer);
+        }
+    }
+}
