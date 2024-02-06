f119d8db7098247ec9ec2d502bbd63998d332729
98SKC <127596280+98SKC@users.noreply.github.com>
2024-02-07 08:49:05 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,30 +1,60 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
+
 public class Main {
-    public static void main(String[] args) throws IOException {
+	    public static void main(String[] args) throws Exception {
 
 
-    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    	StringTokenizer st;
+	    	boolean[][] visit=new boolean[100][100];
+	    	
+	    	int T=Integer.parseInt(br.readLine());
+	    	int count=0;
+	    	int x;
+	    	int y;
+	    	
+	    	for(int square=0;square<T;square++) {
+	    		st=new StringTokenizer(br.readLine());
+	    		y=Integer.parseInt(st.nextToken());//x부터 10
+	    		x=Integer.parseInt(st.nextToken());//y부터 10
+	    		
+	    		//#1 continue
+//	    		for(int i=y;i<y+10;i++) {
+//	    			for(int j=x;j<x+10;j++) {
+//	    				if(visit[i][j]==true) continue;
+//	    				visit[i][j]=true;
+//	    				count++;
+//	    			}
+//	    		}
+	    		
+	    		//#2 if 이후 else
+	    		for(int i=y;i<y+10;i++) {
+	    			for(int j=x;j<x+10;j++) {
+	    				if(visit[i][j]==true) {
+	    					
+	    				}else {
+	    					visit[i][j]=true;
+		    				count++;
+	    				}
 
-    	boolean[][] visit=new boolean[100][100];
-    	
-    	int T=Integer.parseInt(br.readLine());
-    	int count=0;
-    	int row;
-    	int col;
-    	
-    	for(int square=0;square<T;square++) {
-    		String[] str=br.readLine().split(" ");
-    		row=Integer.parseInt(str[0]);
-    		col=Integer.parseInt(str[1]);
-    		for(int i=row;i<row+10;i++) {
-    			for(int j=col;j<col+10;j++) {
-    				if(visit[i][j]==true) continue;
-    				visit[i][j]=true;
-    				count++;
-    			}
-    		}
-    	}
-    	 System.out.println(count);    	
-    }
-}
+	    			}
+	    		}
+	    		
+//	    		//#3 if만
+//	    		for(int i=y;i<y+10;i++) {
+//	    			for(int j=x;j<x+10;j++) {
+//	    				if(visit[i][j]!=true) {
+//	    					visit[i][j]=true;
+//		    				count++;
+//	    				}
+//
+//	    			}
+//	    		}
+
+	    		
+	    	}
+	    	 System.out.println(count);    	
+	    }
+	}
