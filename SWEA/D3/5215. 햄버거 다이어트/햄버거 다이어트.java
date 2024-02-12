712b91bbfddfb1f248e0d5bb5534b6d4f72c1340
98SKC <127596280+98SKC@users.noreply.github.com>
2024-02-12 22:12:07 +0900

    [D3] Title: 햄버거 다이어트, Time: 181 ms, Memory: 20,252 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,47 +1,57 @@
 import java.util.*;
 import java.io.*;
-class Solution
+ 
+ 
+public class Solution
 {
-	static int max;
-	static int limit;
-	static int N;
-	static int[] foodCal;
-	static int[] foodScore;
-	static StringBuilder sb=new StringBuilder();
-	public static void main(String args[]) throws Exception{
+    public static int[] value;
+    public static int[] cal;
+    public static int answer;
+    public static int N;
+    public static int max;
+     
+    public static void main(String args[]) throws Exception
+    {
+         
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb=new StringBuilder();
+        int T=Integer.parseInt(br.readLine());
 
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int T=Integer.parseInt(br.readLine().trim());
-		
-		for(int test_case = 1; test_case <= T; test_case++){
-			
-			StringTokenizer st;
-			max=0;
-			st=new StringTokenizer(br.readLine());
-			N=Integer.parseInt(st.nextToken());
-			limit=Integer.parseInt(st.nextToken());
-			foodCal=new int[N];
-			foodScore=new int[N]; 
-			for(int i=0;i<N;i++) {
-				st=new StringTokenizer(br.readLine());
-				foodScore[i]=Integer.parseInt(st.nextToken());
-				foodCal[i]=Integer.parseInt(st.nextToken());
-			}
-			helper(0, 0, 0);
-			sb.append("#").append(test_case).append(" ").append(max).append("\n");
-		}
-		System.out.println(sb);
-	}
-	
-	public static void helper(int curPos,int curCal,int curScore) {
-		if(curCal>=limit) {return;}
-		if(curPos==N) {
-			max=Math.max(max, curScore);
-			return;
-		}else {
-			helper(curPos+1,curCal+foodCal[curPos],curScore+foodScore[curPos]);// 지금 음식 추가
-			helper(curPos+1,curCal,curScore);//지금 음식 생략
-		}
-
-	}
+        for(int tc = 1; tc <= T; tc++){
+            answer=0;
+            st=new StringTokenizer(br.readLine());
+            N=Integer.parseInt(st.nextToken());
+            max=Integer.parseInt(st.nextToken());
+            value = new int[N];
+            cal = new int[N];
+             
+            for(int i=0;i<N;i++){
+            	 st=new StringTokenizer(br.readLine());
+                 value[i]=Integer.parseInt(st.nextToken());
+                 cal[i]=Integer.parseInt(st.nextToken());
+            }
+            helper(0,0,0);
+             
+            sb.append("#").append(tc).append(" ").append(answer).append("\n"); 
+            
+        }
+        System.out.println(sb);
+ 
+    }
+            public static void helper(int i, int calState, int valueState ){//포함했을때와 안했을 때           
+                if(i==N){// 탐색 범위끝까지 가면
+                    answer=Math.max(valueState,answer);
+                    return;
+                }
+                 
+                helper(i+1,calState,valueState);// 추가하지 않은 경우
+                if(calState+cal[i]>max){
+                    answer=Math.max(valueState,answer);
+                    return;
+                }else{
+                    helper(i+1, calState+cal[i], valueState+value[i]);
+                }
+                 
+        }
 }
