c2ae2acd3734a4df9e20bc70de92f9495f734376
98SKC <127596280+98SKC@users.noreply.github.com>
2024-02-13 15:26:10 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 3,818 ms, Memory: 21,144 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,76 +1,68 @@
 import java.util.*;
 import java.io.*;
-
-
-
+ 
+ 
+ 
 public class Solution {
-	
+     
+    static int[] kZero;
+    static int[] iZero;
+    static int[] sub;
+    static boolean[] v;
+    static int win;
+    
+     
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        StringTokenizer st;
+        int T=Integer.parseInt(br.readLine());
+        HashSet<Integer> set=new HashSet<>();
+        for(int tc=1;tc<=T;tc++) {
+            set.clear();
+            win=0;
+            st=new StringTokenizer(br.readLine());
+            kZero=new int[9];
+            iZero=new int[9];
+            v=new boolean[9];
+            sub=new int[9];
+            for(int i=0;i<9;i++) {
+                kZero[i]=Integer.parseInt(st.nextToken());
+                set.add(kZero[i]);
+            }
+            int count=0;
+            for(int i=1;i<=18;i++) {
+                if(!set.contains(i)) {
+                    iZero[count++]=i;
+                }
+            }
+            perm(0,0);
+            sb.append("#").append(tc).append(" ").append(win).append(" ").append(362880-win).append("\n");
+        }
+        System.out.println(sb);
+    }
+    static void perm(int cnt,int sum) {
+        if(sum>85) {
+        	int total=1;
+        	for(int i=9-cnt;i>0;i--) {
+        		total*=i;
+        	}
+        	win+=total;
+        	return;
+        }
 
-	static List<Integer> kZero;
-	static int[] iZero;
-	static int[] sub;
-	static boolean[] v;
-	static int win;
-	static int lose;
-	
-	
-	public static void main(String[] args) throws Exception{
-		//System.setIn(new FileInputStream("res/s_input.txt"));
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb=new StringBuilder();
-		StringTokenizer st;
-		int T=Integer.parseInt(br.readLine());
-		
-		for(int tc=1;tc<=T;tc++) {
-			kZero=new ArrayList<>();
-			win=0;
-			lose=0;
-			st=new StringTokenizer(br.readLine());
-			iZero=new int[9];
-			v=new boolean[9];
-			sub=new int[9];
-			for(int i=0;i<9;i++) {
-				kZero.add(Integer.parseInt(st.nextToken()));
-			}
-			int count=0;
-			for(int i=1;i<=18;i++) {
-				if(!kZero.contains(i)) {
-					iZero[count++]=i;
-				}
-			}
-			
-			perm(0);
-			sb.append("#").append(tc).append(" ").append(win).append(" ").append(lose).append("\n");
-		}
-		System.out.println(sb);
-	}
-	static void perm(int cnt) {
-		if(cnt==9) {
-			int kWin=0;
-			int iWin=0;
-			for(int i=0;i<9;i++) {
-				if(kZero.get(i)-sub[i]>0) {
-					kWin+=kZero.get(i)+sub[i];
-				}else {
-					iWin+=kZero.get(i)+sub[i];
-				}
-			}
-			if(kWin>iWin) {
-				win++;
-			}else if(kWin<iWin) {
-				lose++;
-			}
-			return;
-		}
-		
-		for(int i=0;i<9;i++) {
-			if(v[i]) continue;
-			v[i]=true;
-			sub[cnt]=iZero[i];
-			perm(cnt+1);
-			v[i]=false;
-		}
-	}
-
-
+     
+        for(int i=0;i<9;i++) {
+            if(v[i]) continue;
+            v[i]=true;
+            if(kZero[cnt]>iZero[i]) {
+            	perm(cnt+1,sum+iZero[i]+kZero[cnt]);
+            }else {
+            	perm(cnt+1,sum);
+            }
+            v[i]=false;
+        }
+    }
+ 
+ 
 }
