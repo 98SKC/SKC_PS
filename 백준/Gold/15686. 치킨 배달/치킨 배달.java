af2bab5ce053eff90727ea423ff4a77718dd3eab
98SKC <127596280+98SKC@users.noreply.github.com>
2024-02-13 10:37:38 +0900

    [Gold V] Title: 치킨 배달, Time: 220 ms, Memory: 17636 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -3,12 +3,13 @@ import java.util.*;
 
 public class Main {
     static int n, m; // n: 도시의 크기, m: 선택할 수 있는 최대 치킨집의 수
-    static int[][] map; // 도시의 지도를 저장하는 2차원 배열
+
     static ArrayList<int[]> house = new ArrayList<>(); // 모든 집의 위치를 저장하는 배열 리스트
     static ArrayList<int[]> chicken = new ArrayList<>(); // 모든 치킨집의 위치를 저장하는 배열 리스트
-    static ArrayList<int[]> choice = new ArrayList<>(); // 선택된 치킨집의 위치를 저장하는 배열 리스트
+   // static ArrayList<int[]> choice = new ArrayList<>(); // 선택된 치킨집의 위치를 저장하는 배열 리스트
+    static int[][] choice; 
+    
     static int result = Integer.MAX_VALUE; // 최소 치킨 거리를 저장하는 변수
-    static boolean[] visit; // 백트래킹 중 치킨집의 방문 여부를 체크하는 배열
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,37 +19,37 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        choice=new int[m][2];
+        int sub=0;
 
         // 도시의 지도를 제작. 0은 빈 칸, 1은 집, 2는 치킨집을 나타낸다.
-        map = new int[n][n];
+       // map = new int[n][n];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                sub= Integer.parseInt(st.nextToken());
  
                 // 집의 위치를 house 리스트에 저장.
-                if (map[i][j] == 1) {
+                if (sub == 1) {
                     house.add(new int[]{i, j});
                 }
     
                 // 치킨집의 위치를 chicken 리스트에 저장. 
-                if (map[i][j] == 2) {
+                if (sub == 2) {
                     chicken.add(new int[]{i, j});
                 }
             }
         }
 
-        // 치킨집 방문 여부를 확인하기 위한 배열을 초기화. 초기값은 false
-        visit = new boolean[chicken.size()];
-
         // 백트래킹을 시작하여 최소 치킨 거리를 계산.
-        back(0, 0);
+        DFS(0, 0);
         System.out.println(result); // 계산된 최소 치킨 거리를 출력.
 
     }
 
-    // 백트래킹 메소드. depth는 현재 선택된 치킨집의 개수, start는 탐색을 시작할 치킨집의 인덱스.
-    static void back(int depth, int start) {
+    // depth는 현재 선택된 치킨집의 개수, start는 탐색을 시작할 치킨집의 인덱스.
+    static void DFS(int depth, int start) {
+    	//모든 치킨집을 고르면
         if (depth == m) {
            
             // 선택된 치킨집이 m개일 때, 모든 집에 대한 치킨 거리의 합을 계산.
@@ -69,15 +70,10 @@ public class Main {
             return;
         }
 
-        // 가능한 모든 치킨집 조합을 만들기 위해 백트래킹을 수행.
+        // 가능한 모든 치킨집 조합을 만들기 위해 재귀 수행.
         for (int i = start; i < chicken.size(); i++) {
-            if (!visit[i]) {
-                visit[i] = true; // 현재 치킨집을 방문한 것으로 표시한다.
-                choice.add(chicken.get(i)); // 현재 치킨집을 선택된 치킨집 목록에 추가한다.
-                back(depth + 1, i + 1); // 다음 치킨집을 선택하기 위해 재귀 호출.
-                choice.remove(choice.size() - 1); // 백트래킹을 위해 마지막으로 추가된 치킨집을 제거.
-                visit[i] = false; // 방문 표시를 해제.
-            }
-        }
+            choice[depth]=chicken.get(i); // 현재 치킨집을 선택된 치킨집 목록에 추가한다.
+            DFS(depth + 1, i + 1); // 다음 치킨집을 선택하기 위해 재귀 호출.
+      }
     }
 }
