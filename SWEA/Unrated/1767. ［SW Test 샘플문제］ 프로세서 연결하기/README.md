c35520b2b7eba760c32f958f1064ba21c5cbaf43
98SKC <127596280+98SKC@users.noreply.github.com>
2024-02-28 11:56:33 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 711 ms, Memory: 90,196 KB -BaekjoonHub

---
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [SW Test 샘플문제] 프로세서 연결하기 - 1767 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV4suNtaXFEDFAUf) 
+
+### 성능 요약
+
+메모리: 90,196 KB, 시간: 711 ms, 코드길이: 3,688 Bytes
+
+### 제출 일자
+
+2024-02-28 11:56
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -0,0 +1,145 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution
+
+{
+	static int[][] processer;
+	static int N;
+	static int min;
+	static int max=0;// 프로세서 개수
+	static List<Integer> position;
+	static final int[] di= {1,0,-1,0};//0-하. 1-우. 2-상, 3-좌
+	static final int[] dj= {0,1,0,-1};
+	static boolean[][] map;
+	
+    public static void main(String args[]) throws Exception{
+    
+    	//System.setIn(new FileInputStream("res/sample_input (9).txt"));
+    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	StringBuilder sb=new StringBuilder();
+    	int T=Integer.parseInt(br.readLine());
+    
+    	
+    	for(int tc=1;tc<=T;tc++) {
+    		position=new ArrayList<>();
+    		N=Integer.parseInt(br.readLine());  		
+    		map=new boolean[N][N];
+    		min=Integer.MAX_VALUE;
+    		for(int i=0;i<N;i++) {
+    			st=new StringTokenizer(br.readLine());
+    			for(int j=0;j<N;j++) {
+        			if(st.nextToken().charAt(0)=='1') {
+        				map[i][j]=true;
+        				if(i!=0&&i!=N-1&&j!=0&&j!=N-1) {
+        					position.add(i*N+j);
+        				}
+        			}
+        		}
+    		}
+    		
+    		helper(0,0,map,max);
+    		
+    		sb.append("#").append(tc).append(" ").append(min).append("\n");
+    	}
+    	
+    System.out.println(sb);
+    }
+    
+    
+    
+    public static void helper(int cnt,int answer,boolean[][] visit,int have) {
+    	boolean ch=true;
+    	if(cnt==position.size()) {
+    		if(have>max) {
+    			max=have;
+    			min=answer;
+    		}else if(have==max) {
+    			min=Math.min(min, answer);
+    		}
+    		return;
+    	}
+    	int sub=0;
+    	boolean[][] v=new boolean[N][N];
+    	//배열 복사
+    	for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+    			v[i][j]=visit[i][j];
+    		}
+		}
+    	
+    	for(int a=0;a<4;a++) {
+    		sub=check(cnt,a,v);
+    		if(sub!=0) {
+    			ch=false;
+    			//System.out.printf("%d, %d 좌표 %d 방향 %d만큼 찍었음\n",position.get(cnt)/N,position.get(cnt)%N,a,sub);
+    			helper(cnt+1,answer+sub,v,have+1);
+    			back(cnt,sub,a,v);
+    			
+    		}
+//    		else {
+//    			System.out.println("불가능. 방향 전환");
+//    		}
+    	}
+    	 
+    		helper(cnt+1,answer,v,have);
+    	
+    	
+    }
+    static void back(int cnt,int sub,int dir, boolean[][] visit) {
+    	
+    	int ni=position.get(cnt)/N;
+    	int nj=position.get(cnt)%N;
+    	//System.out.printf("%d, %d 좌표 %d 방향 %d만큼 돌리기\n",ni,nj,dir,sub);
+    	while(sub>0) {// 전선 설치가 불가능하면 원래대로
+			ni=ni+di[dir];
+    		nj=nj+dj[dir];
+			visit[ni][nj]=false;
+			sub--;
+		}
+    }
+    static int check(int cnt,int dir, boolean[][] visit) {
+    	//System.out.printf("들어온 좌표 %d %d, 뱡향 %d\n",position.get(cnt)/N,position.get(cnt)%N,dir);
+    	int ni=position.get(cnt)/N+di[dir];
+    	int nj=position.get(cnt)%N+dj[dir];
+    	int count=0;
+    	if(dir%2==0) {//세로
+    		while(ni>=0&&ni<N) {		
+        		if(!visit[ni][nj]) {
+        			visit[ni][nj]=true;
+        			count++;
+        		}else {
+        			while(count>0) {// 전선 설치가 불가능하면 원래대로
+        				ni=ni-di[dir];
+                		nj=nj-dj[dir];
+        				visit[ni][nj]=false;
+        				count--;
+        			}
+        			return 0;
+        		}
+        		ni=ni+di[dir];
+        		nj=nj+dj[dir];
+    		}
+    		
+    	}else {
+    		while(nj>=0&&nj<N) {
+        		if(!visit[ni][nj]) {
+        			visit[ni][nj]=true;
+        			count++;
+        		}else {
+        			while(count>0) {// 전선 설치가 불가능하면 원래대로
+        				ni=ni-di[dir];
+                		nj=nj-dj[dir];
+        				visit[ni][nj]=false;
+        				count--;
+        			}
+        			return 0;
+        		}
+        		ni=ni+di[dir];
+        		nj=nj+dj[dir];
+    		}
+    	}
+    	return count;
+    }
+}
