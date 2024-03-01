d3a00639a7ec4922697bd6574c2f017f028e840d
98SKC <127596280+98SKC@users.noreply.github.com>
2024-03-02 01:03:51 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 516 ms, Memory: 44824 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
@@ -50,14 +50,13 @@ public class Main {
       }
     
     static void distributionSand(int i,int j, int dir, int sand) {//sub는 원래 있던 양.
-    	//System.out.println("이동횟수: "+count++);
+   
 
     	int[] di;
     	int[] dj;
     	int ni;
     	int nj;
     	int alpha=sand;
-    	//System.out.println("alpha가 여기까진 1?"+ alpha);
     	int sub;
     	
     	check=new boolean[10];
@@ -81,23 +80,17 @@ public class Main {
     			check[a]=true;
     			cnt++;
     		}
-//    		else {
-//    			System.out.println("버려지는 곳: "+a);
-//    		}
     	}
     
-    	
-    	//여기서 알 수 있는건 토네이도는 존나 잘가고 있다.
-    	//System.out.printf("x좌표 i: %d, j:%d \n " ,i,j);
+
     	int ccnt=10-cnt;
-    	//System.out.println("토네이도가 여기 도착하면 버려지는 칸이 "+ccnt+"개");
     	map[i][j]=0;//토네이도가 위치한 곳은 다 없엠.
 		for (int a = 0; a < 9; a++) {
 
 			ni = i + di[a];
 			nj = j + dj[a];
 			sub =(int) (sand * (rp[a]*0.01));
-			//System.out.println("sub는 "+sub);
+
 			
 
 			if (check[a]) {
@@ -105,7 +98,6 @@ public class Main {
 				
 			} else {
 				
-//				System.out.printf("토네이도가 i: %d ,j:%d 에 도착하고  ni: %d  nj:%d 에 %d만큼 버려짐 \n",i,j,ni,nj,sub);
 				answer += sub;
 			}
 			alpha -= sub;
@@ -114,29 +106,15 @@ public class Main {
 		ni=i+di[9];
 		nj=j+dj[9];
     	if(check[9]) {
-//    		ni=i+di[9];
-//			nj=j+dj[9];
-//    		System.out.printf("ni %d  nj %d ",ni,nj);// nj가 틀렸네.
-//    		System.out.println();
 			map[ni][nj]+=alpha;	
-			//System.out.printf("모래가 i: %d, j:%d 위치로 이동. map[ni][nj]는 %d \n  ",ni,nj,map[ni][nj]);
     	}else {
-    		//System.out.printf("토네이도가 i: %d ,j:%d 에 도착하고  ni: %d  nj:%d 에 %d만큼 버려짐. 버려지는게 알파임 \n",i,j,ni,nj,alpha);
     		answer+=alpha;
     	}
-//    	int ada=map[i][j];
-//    	map[i][j]=-1;
-//    	for(int[] a:map) {
-//    		System.out.println(Arrays.toString(a));
-//    	}
-//    	map[i][j]=ada;
-//    	System.out.println("-----------------------------");
+
     }
   
     static void movingTonado() {
-    	
-    	
-    	
+
     	int ni=N/2;
     	int nj=N/2;
     	int dir=-1;
@@ -146,14 +124,14 @@ public class Main {
     	while(escape) {
     		
     		dir=(dir+1)%4;
-    		//System.out.printf("처음 ni %d,  nj %d, 현 dir %d,  a: %d \n",ni,nj, dir, a);
+    		
     	
     		switch(dir) {
     		
     			case 0:
     				a++;
     				for(int c=0;c<a;c++) {
-    					//System.out.printf("x좌표 i: %d, j:%d \n " ,ni,nj);
+    					
     					nj-=1;
     					if(ni==0&&nj==-1) {
     						escape=false;
@@ -165,18 +143,14 @@ public class Main {
     			case 1:
     				b++;
     				for(int c=0;c<b;c++) {
-    					//System.out.printf("x좌표 i: %d, j:%d \n" ,ni,nj);
     					ni+=1;
-    				//	System.out.printf("ni는 %d  nj는 %d\n",ni,nj);
     					distributionSand(ni,nj,dir,map[ni][nj]);
     				}
     				break;
     			case 2:
     				a++;
     				for(int c=0;c<a;c++) {
-    					//System.out.printf("x좌표 i: %d, j:%d \n " ,ni,nj);
     					nj+=1;
-    					//System.out.println("1인지 확인"+map[ni][nj]);
     					distributionSand(ni,nj,dir,map[ni][nj]);
     					
     				}
@@ -184,17 +158,14 @@ public class Main {
     			case 3:
     				b++;
     				for(int c=0;c<b;c++) {
-    					//System.out.printf("x좌표 i: %d, j:%d \n" ,ni,nj);
     					ni-=1;
     					distributionSand(ni,nj,dir,map[ni][nj]);
     				}
-    			
-    			
+
     		}
     		
     	}
-    	
-    	
+
     }
    
 }
