9edacf817c94bcbfd6ba1082cf76c2ff5a56eabe
98SKC <127596280+98SKC@users.noreply.github.com>
2024-03-09 23:21:52 +0900

    [Silver I] Title: 지름길, Time: 84 ms, Memory: 11692 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -1,95 +1,71 @@
 import java.util.*;
 import java.io.*;
+import java.nio.BufferOverflowException;
+
 
 public class Main {
-  
-    static int[] arr;// 내가 채워야 하는 노드
-    static boolean[] check;// 최소거리. true가 되어야 도착. 확정
-    static List<Node>[] list;// 주어지는 가중치
-    static int N,goal,answer=0;
+
+    static int[] arr;
+    static boolean[] check;
+
+    public static final int INF = Integer.MAX_VALUE;
+
+
+    static int N, M;
     static int D=10000;
-    static StringBuilder sb = new StringBuilder();
-
-
-    public static void main(String[] args)throws Exception {
-        
+    static ArrayList<Node>[] list;
+    static int[] dp;
+    public static void main(String[] args)throws IOException {
+        int i, j, z, t;
         StringTokenizer st;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
-        goal=Integer.parseInt(st.nextToken());
-        
-        arr = new int[D+1];
-        check = new boolean[D+1];
-        list = new ArrayList[D+1];
-        
-        Arrays.fill(arr, Integer.MAX_VALUE);
-        
-        for(int i = 0; i <= D; i++){
+        M = Integer.parseInt(st.nextToken());
+
+        dp = new int[M + 1];
+        list = new ArrayList[M + 1];
+        for (i = 0; i <= M; i++) {
             list[i] = new ArrayList<>();
         }
-        
-//        for(int i=0;i<D;i++) {
-//        	list[i].add(new Node(i+1,1));
-//        }//  기본 간선 가중치 1.
-        
-        for(int i=0;i<N;i++) {
-           st = new StringTokenizer(br.readLine());
-           int before = Integer.parseInt(st.nextToken());
-           int after = Integer.parseInt(st.nextToken());
-           int num = Integer.parseInt(st.nextToken());
-           
-           list[before].add(new Node(after,num));
+
+        for (i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            if (end > M)
+                continue;
+
+            list[end].add(new Node(start, weight));
+        }
+
+        for (i = 1; i <= M; i++) {
+            dp[i] = dp[i - 1]+1;
+            for (Node node : list[i]) {
+            	dp[i]=Math.min(dp[i], dp[node.end] + node.weight);
+            }
         }
         
-        dijkstra();
-        System.out.println(answer);
-    }
-    
-    public static void dijkstra() {
-       PriorityQueue<Node> queue = new PriorityQueue<>();
-       queue.add(new Node(0,0));
-       arr[0] = 0;
-       
-       while(!queue.isEmpty()) {
-          Node n = queue.poll();
-          int end = n.end;
-          
-          
-          if(check[end]) continue;
-          
-          check[end] = true;
-          
-          if(end==goal) {
-        	  answer=arr[end];
-        	  break;
-          }
-          list[end].add(new Node(end+1,1));
-          for(Node node : list[end]) {
-             if(arr[node.end] > arr[end] + node.weight) {
-                arr[node.end] = arr[end] + node.weight;
-                queue.add(new Node(node.end,arr[node.end]));
-             }
-          }
-          
-       }
-       
+        System.out.println(dp[M]);
     }
 
 }
 
-class Node implements Comparable<Node>{
-   public int end;
-   public int weight;
-   
-   Node(int end,int weight){
-      this.end = end;
-      this.weight = weight;
-   }
-   @Override
-    public int compareTo(Node o) {
+class Node implements Comparable<Node> {
+    int end;
+    int weight;
+
+    Node(int end, int weight) {
+        this.end = end;
+        this.weight = weight;
+    }
+
+    @Override
+        public int compareTo(Node o) {
+        // TODO Auto-generated method stub
         return weight - o.weight;
     }
- 
 }
