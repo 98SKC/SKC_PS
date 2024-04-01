1aab44343345aec617cc105083fde53de59fda74
98SKC <127596280+98SKC@users.noreply.github.com>
2024-04-01 17:07:27 +0900

    [Bronze V] Title: Hello World, Time: 68 ms, Memory: 11288 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.awt.List;
+import java.io.*;
+
+public class Main {
+  
+
+	//static StringBuilder sb=new StringBuilder();
+	
+    public static void main(String[] args) throws Exception {
+
+
+       System.out.println("Hello World!");
+       
+    }
+
+}
