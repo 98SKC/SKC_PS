ac72b8940d49ba56acf2e25ce45b895f351c34b5
98SKC <127596280+98SKC@users.noreply.github.com>
2024-04-06 22:26:50 +0900

    [Bronze V] Title: N 찍기, Time: 208 ms, Memory: 29720 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -3,11 +3,15 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
+
+        
 		int i = 1;
 		while(i<= N) {
-			System.out.println(i++);
-		}	
+            sb.append(i+"\n");
+			i++;
+		}
+        System.out.println(sb);
 	}
 }
