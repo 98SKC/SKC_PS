805075f909f5ef21cf1977f311c2ad432ed1b4a0
98SKC <127596280+98SKC@users.noreply.github.com>
2024-04-12 08:59:52 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 472 ms, Memory: 61756 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,38 +1,36 @@
-import com.sun.jdi.IntegerType;
-
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
-
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder();
+		HashMap<Integer, String> hash1 = new HashMap<Integer, String>();
+		HashMap<String, Integer> hash2 = new HashMap<String, Integer>();
+		
+		
+		for(int i = 1; i <= N; i++) {
+			String S = br.readLine();
+			hash1.put(i, S);
+			hash2.put(S, i);
+		}
+		
+		for(int i = 0; i < M; i++) {
+			String S = br.readLine();
+			if(49 <= S.charAt(0) && S.charAt(0) <= 57) {
+				sb.append(hash1.get(Integer.parseInt(S))).append("\n");
+			}else {
+				sb.append(hash2.get(S)).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        HashMap<String, Integer> numberOutput=new HashMap<>();
-        HashMap<Integer, String> stringOutput=new HashMap<>();
-        String sub;
-
-        String[] str=br.readLine().split(" ");
-
-        int N= Integer.parseInt(str[0]);
-        int M= Integer.parseInt(str[1]);
-
-        for(int i=1;i<=N;i++){
-            sub=br.readLine();
-            numberOutput.put(sub,i);
-            stringOutput.put(i,sub);
-        }
-        for(int j=0;j<M;j++){
-            sub=br.readLine();
-            if(49 <= sub.charAt(0) && sub.charAt(0) <= 57){
-                System.out.println(stringOutput.get(Integer.parseInt(sub)));
-            }else{
-                System.out.println(numberOutput.get(sub));
-            }
-
-        }
-    }
 }
