e45fbb37c08b94ea726cf4d82a457ae1b24198fa
98SKC <127596280+98SKC@users.noreply.github.com>
2024-04-23 01:02:18 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 156 ms, Memory: 13576 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main {
 
 	static int[][] egg;
-	static boolean[] check;
+	//static boolean[] check;
 	static int N;
 	static int answer=0;
 	static boolean find;
@@ -13,7 +13,7 @@ public class Main {
 	        N = Integer.parseInt(br.readLine());
 	        StringTokenizer st;
 	        egg = new int[N][2];
-	        check = new boolean[N];
+	       // check = new boolean[N];
 
 	        for (int i = 0; i < N; i++) {
 	            st = new StringTokenizer(br.readLine());
@@ -27,17 +27,16 @@ public class Main {
 
 	    static void backtrack(int pos, int broken) {
 	    	if ((N - pos)*2 + broken<= answer) return;
-	
-	    	if (pos == N) {
-	            answer = Math.max(answer, broken);
-	            return;
-	        }
-	        if (check[pos]) {
+	    	answer = Math.max(answer, broken);
+	    	if (pos == N) return;
+	        
+	       // if (check[pos]) {
+	    	if (egg[pos][0] <= 0) {
 	            backtrack(pos + 1,broken);  // 현재 계란이 이미 깨졌다면 다음 계란으로
 	        } else {
 	            boolean isHit = false;
 	            for (int i = 0; i < N; i++) {
-	                if (i != pos && !check[i]) {
+	                if (i == pos || egg[i][0] <= 0) continue;
 	                    isHit = true;
 	                    int cnt=0;
 	                    // 계란을 서로 치기
@@ -45,20 +44,20 @@ public class Main {
 	                    egg[i][0] -= egg[pos][1];
 
 	                    if (egg[pos][0] <= 0) {
-	                    	check[pos] = true;
+	                    	//check[pos] = true;
 	                    	cnt+=1;
 	                    } 
 	                    if (egg[i][0] <= 0) {
-	                    	check[i] = true;
+	                    	//check[i] = true;
 	                    	cnt+=1;
 	                    } 
 	                    backtrack(pos + 1,broken+cnt);
 	                    // 원상 복구
-	                    check[pos] = false;
-	                    check[i] = false;
+	                   // check[pos] = false;
+	                   // check[i] = false;
 	                    egg[pos][0] += egg[i][1];
 	                    egg[i][0] += egg[pos][1];
-	                }
+	                
 	            }
 	            if (!isHit) {  // 다른 계란을 치지 못했다면 다음 계란으로
 	                backtrack(pos + 1,broken);
