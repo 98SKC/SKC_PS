f3c4a26b2ab02ee1c346f2becabebc7fab4a1d9f
98SKC <127596280+98SKC@users.noreply.github.com>
2024-05-07 01:12:58 +0900

    [Gold V] Title: 순열의 순서, Time: 76 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Gold/1722. 순열의 순서/순열의 순서.java
+++ 백준/Gold/1722. 순열의 순서/순열의 순서.java
@@ -1,54 +1,73 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        long [] f = new long[21];
-        boolean [] c = new boolean[21]; //n엔 중복된 수가 없으므로 중복을 없앨 boolean 배열
-        Arrays.fill(f, 1);
-        
-        for(int i=1; i<=20; i++) { //팩토리얼 구하기
-            f[i] = f[i-1]*i;
+    static int N;
+    static long[] fac = new long[21];
+    static int[] goal;
+    static boolean[] v;
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int oper = Integer.parseInt(st.nextToken());
+
+        fac[0] = 1;
+        for (int i = 1; i <= 20; i++) {
+            fac[i] = fac[i - 1] * i;
         }
+        //System.out.println(Arrays.toString(fac));
         
-        int n = scan.nextInt();
-        int what = scan.nextInt();
-        
-        int [] a = new int[n];
-        if(what == 2) { //몇 번째 순열인지
-            for(int i=0; i<n; i++)
-                a[i] = scan.nextInt();
-            
-            long ans = 1; //순열은 1 번째 부터 시작
-            for(int i=0; i<n; i++) {
-                for(int j=1; j<a[i]; j++) {
-                    if(!c[j])
-                        ans += f[n-i-1];
-                }
-                c[a[i]]=true;
+        if (oper == 1) {
+        	
+            long goal2;
+            goal2 = Long.parseLong(st.nextToken());
+            oper1(goal2);
+        } else {
+            goal = new int[N];
+            for (int i = 0; i < N; i++) {
+                goal[i] = Integer.parseInt(st.nextToken());
             }
-            System.out.println(ans);
-        }
-        else if(what == 1) { //k 번째 순열 출력
-            long k = scan.nextLong();
-            for(int i=0; i<n; i++) {
-                for(int j=1; j<=n; j++) {
-                    if(c[j])
-                        continue;
-                    if(f[n-i-1] < k) {
-                        k -= f[n-i-1];
-                    }
-                    else {
-                        a[i] = j;
-                        c[j] = true;
-                        break;
-                    }
-                }
-            }
-            for(int i=0; i<n; i++) {
-                System.out.print(a[i] + " ");
-            }
+            oper2();
         }
     }
+
+    static void oper1(long goal) {
+        int[] answer = new int[N];
+        v = new boolean[N + 1];
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (v[j]) continue;
+                if (fac[N - i - 1] < goal) {
+                    goal -= fac[N - i - 1];
+                } else {
+                    answer[i] = j;
+                    v[j] = true;
+                    break;
+                }
+            }
+        }
+        for(int i=0; i<N; i++) {
+            System.out.print(answer[i] + " ");
+        }
+        System.out.println();
+    }
+
     
+    static void oper2() {
+        long answer = 1;
+        v = new boolean[N + 1];
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j < goal[i]; j++) {
+                if (!v[j]) {
+                    answer += fac[N - i - 1];
+                }
+            }
+            v[goal[i]] = true;
+        }
+        System.out.println(answer);
+    }
 }
