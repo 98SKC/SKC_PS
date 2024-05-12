9e054cb13043fc572482affc3bfc86a9fea78772
98SKC <127596280+98SKC@users.noreply.github.com>
2024-05-12 20:15:19 +0900

    [Bronze V] Title: 웰컴, Time: 72 ms, Memory: 11180 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/."); // 이스케이프 처리된 \ 문자
+    }
+}
