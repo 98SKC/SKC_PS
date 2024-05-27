e4c714e248f2d1d054a5a233756aeda9d517a7e7
98SKC <127596280+98SKC@users.noreply.github.com>
2024-05-27 15:18:47 +0900

    [Gold V] Title: AC, Time: 792 ms, Memory: 112728 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -5,56 +5,71 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
+        int T=Integer.parseInt(br.readLine());
+        Deque<Integer> dq;
+        boolean r;
+        int N;
+        int[] arr;
+        String sub;
+        String[] subArr;
+        char c;
+        StringBuilder sb=new StringBuilder();
+        StringBuilder answer=new StringBuilder();
+        StringTokenizer st;
+        for(int test_case=1;test_case<=T;test_case++) {
+        	boolean isError=false;
+        	String str=br.readLine();
+        	N=Integer.parseInt(br.readLine());
+        	arr=new int[N];
+        	sub=br.readLine();
+        	sub=sub.replace("[", "");
+        	sub=sub.replace("]", "");
+        	subArr=sub.split(",");
+        	dq=new ArrayDeque<Integer>();
+        	for(int i=0;i<N;i++) {
+        		dq.add(Integer.parseInt(subArr[i]));
+        	}
+        	
+        	r=false;
+        	for(int i=0;i<str.length();i++) {
+        		c=str.charAt(i);
+        		if(c=='R') {
+        			r=!r;
+        		}else {
+        			if(dq.isEmpty()) {
+        				isError=true;
+        				sb.append("error"+"\n");
+        				break;
+        			}
+        			if(r) {
+        				dq.pollLast();
+        			}else{
+        				dq.pollFirst();
+        			}
+        			
+        		}
+        		
+        	}
+    		if(!isError) {
+    			sb.append("[");
+    			while(!dq.isEmpty()) {
+    				if(r) {
+    					sb.append(dq.pollLast());
+        				if(!dq.isEmpty()) {
+        					sb.append(",");
+        				}
+    				}else {
+    					sb.append(dq.pollFirst());
+        				if(!dq.isEmpty()) {
+        					sb.append(",");
+        				}
+    				}
+    				
+    			}
+    			sb.append("]"+"\n");
+    		}
 
-        for (int test_case = 1; test_case <= T; test_case++) {
-            String str = br.readLine();
-            int N = Integer.parseInt(br.readLine());
-            String sub = br.readLine();
-            sub = sub.replace("[", "").replace("]", "");
-            String[] subArr = sub.split(",");
-            Deque<Integer> dq = new ArrayDeque<>();
-            for (int i = 0; i < N; i++) {
-                dq.add(Integer.parseInt(subArr[i]));
-            }
-
-            boolean r = false;
-            boolean isError = false;
-
-            for (char c : str.toCharArray()) {
-                if (c == 'R') {
-                    r = !r;
-                } else {
-                    if (dq.isEmpty()) {
-                        sb.append("error\n");
-                        isError = true;
-                        break;
-                    }
-                    if (r) {
-                        dq.pollLast();
-                    } else {
-                        dq.pollFirst();
-                    }
-                }
-            }
-
-            if (!isError) {
-                sb.append("[");
-                while (!dq.isEmpty()) {
-                    if (r) {
-                        sb.append(dq.pollLast());
-                    } else {
-                        sb.append(dq.pollFirst());
-                    }
-                    if (!dq.isEmpty()) {
-                        sb.append(",");
-                    }
-                }
-                sb.append("]\n");
-            }
         }
-
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
