ac9e0429b7f778d5fa21e29ad87df709cd035e72
98SKC <127596280+98SKC@users.noreply.github.com>
2024-06-12 16:15:25 +0900

    [Silver III] Title: 주유소, Time: 364 ms, Memory: 47140 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -8,26 +8,38 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N=Integer.parseInt(br.readLine());
         int[] price=new int[N];
-        int[] dis=new int[N];
+        long[] dis=new long[N];
         String[] distance=br.readLine().split(" ");
         String[] oil=br.readLine().split(" ");
         int min=Integer.MAX_VALUE;
+        Queue<Integer> q=new ArrayDeque<Integer>();
         
-        
-        for(int i=0;i<N-1;i++) {
+        for(int i=0;i<N;i++) {
         	price[i]=Integer.parseInt(oil[i]);
-        	dis[i]=Integer.parseInt(distance[i]);
-        }
-        
-        long total=0;;
-        for(int i=0;i<N-1;i++) {
         	if(price[i]<min) {
+        		q.add(i);// 최소가 갱신되는 노드의 번호는 i
         		min=price[i];
         	}
-        	total+=min*dis[i];
+        	if(i!=0) {
+        		dis[i]=Integer.parseInt(distance[i-1])+dis[i-1];
+        	}else {
+        		dis[i]=0; 	
+        	}
+        	
         }
-        System.out.println(total);
 
+        int before=q.poll();
+        int node=0;
+        long answer=0;
+        while(!q.isEmpty()) {
+        	node=q.poll();
+        	answer+=(dis[node]-dis[before])*price[before];
+        	before=node;
+        }
+        
+        answer+=price[before]*(dis[N-1]-dis[node]);
+        System.out.println(answer);
+        
         
     }
 }
