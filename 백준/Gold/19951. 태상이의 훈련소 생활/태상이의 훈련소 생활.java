cf6d84e35d748a1a59c9a1c0ce9de33529b3aeb1
98SKC <127596280+98SKC@users.noreply.github.com>
2024-06-29 19:48:10 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 500 ms, Memory: 75532 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -6,16 +6,15 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
-        
+        StringBuilder sb=new StringBuilder();
         int N=Integer.parseInt(st.nextToken());
         int M=Integer.parseInt(st.nextToken());
         
         int[] map=new int[N+1];
-        int[] prefix=new int[N+1];
+        int[] prefix=new int[N+2];
         st=new StringTokenizer(br.readLine());
         for(int i=1;i<=N;i++) {
         	map[i]=Integer.parseInt(st.nextToken());
-   
         }
         int start,end,H;
         
@@ -25,20 +24,17 @@ public class Main {
             end=Integer.parseInt(st.nextToken());
             H=Integer.parseInt(st.nextToken());
             prefix[start]+=H;
-            if(end<N) prefix[end+1]-=H;
+            prefix[end+1]-=H;
             
         }
         
         for(int i=1;i<=N;i++) {
-   
         	prefix[i]+=prefix[i-1];
+        	sb.append((map[i]+prefix[i])+" ");
         	
         }
-        
-        for(int i=1;i<=N;i++) {
-	   
-        	System.out.print((map[i]+prefix[i])+" ");
-        }
+        System.out.println(sb);
+
         
     }
 }
