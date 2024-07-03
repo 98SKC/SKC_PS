1a72fd7e9371e61f115d7c7f60d68cc0d3ee212b
98SKC <127596280+98SKC@users.noreply.github.com>
2024-07-03 13:11:09 +0900

    [Gold V] Title: 개똥벌레, Time: 416 ms, Memory: 35748 KB -BaekjoonHub

---
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        int N=Integer.parseInt(st.nextToken());
+        int H=Integer.parseInt(st.nextToken());
+
+        int up=H;
+        int douwn=1;
+        int sub;
+        int[] prefix=new int[H+1];
+
+        for(int i=0;i<N;i++){
+            sub=Integer.parseInt(br.readLine());
+            if(i%2==0){// 아래에서 위로
+                prefix[1]++;
+                if(sub<H){
+                    prefix[sub+1]--;
+                }
+            }else{//위에서 아래로
+                prefix[H-sub+1]++;
+            }
+        }
+
+
+        for(int i=1;i<=H;i++){
+            prefix[i]+=prefix[i-1];
+
+        }
+
+        Arrays.sort(prefix);
+        int min=prefix[1];
+        int count=1;
+        for(int i=2;i<=H;i++){
+            if(prefix[i]==min) count++;
+            else break;
+        }
+        System.out.println(min);
+        System.out.println(count);
+
+
+        //System.out.println("hello");
+    }
+
+}
