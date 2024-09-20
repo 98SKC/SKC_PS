0af1525eaa36382e8789160b0fba5ddb575cdb53
98SKC <127596280+98SKC@users.noreply.github.com>
2024-09-20 09:32:48 +0900

    [Silver V] Title: 올림픽, Time: 60 ms, Memory: 11392 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static class Medal implements Comparable<Medal>{
+        int number;
+        int gold;
+        int silver;
+        int brz;
+
+        public Medal(int number, int gold, int silver, int brz){
+            this.number=number;
+            this.gold=gold;
+            this.silver=silver;
+            this.brz=brz;
+        }
+
+        @Override
+        public int compareTo(Medal m){
+            if(gold==m.gold){
+                if(silver==m.silver){
+                    return m.brz-this.brz;
+                }else{
+                    return m.silver-this.silver;
+                }
+            }else{
+                    return m.gold-this.gold;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception{
+
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        int N=Integer.parseInt(st.nextToken());
+        int K=Integer.parseInt(st.nextToken());
+        int num,gold,silver,brz;
+        Medal[] medal=new Medal[N];
+
+        for(int i=0;i<N;i++){
+            st=new StringTokenizer(br.readLine());
+            num=Integer.parseInt(st.nextToken());
+            gold=Integer.parseInt(st.nextToken());
+            silver=Integer.parseInt(st.nextToken());
+            brz=Integer.parseInt(st.nextToken());
+            medal[i]=new Medal(num,gold,silver,brz);
+
+        }
+        int answer=0;
+        Arrays.sort(medal);
+        for(int i=0;i<N;i++){
+            if(medal[i].number==K){
+                answer=i;
+                break;
+            }
+        }
+        System.out.println(answer);
+
+
+    }
+
+}
