ce0861f3df558763ee0c49be7dfe6e4dea7fb13a
98SKC <127596280+98SKC@users.noreply.github.com>
2025-01-03 18:22:21 +0900

    [Silver V] Title: 종이자르기, Time: 64 ms, Memory: 11648 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.java
+++ 백준/Silver/2628. 종이자르기/종이자르기.java
@@ -4,44 +4,46 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        
-        int N=Integer.parseInt(st.nextToken());
-        int M=Integer.parseInt(st.nextToken());
-        int max=0;
-        int K=Integer.parseInt(br.readLine());
-        ArrayList<Integer> row=new ArrayList<>();
-        ArrayList<Integer> col=new ArrayList<>();
-        int sub;
-        row.add(0);
-        col.add(0);
-        row.add(N);
-        col.add(M);
-        
-        for(int i=0;i<K;i++) {
-        	st=new StringTokenizer(br.readLine());
-        	sub=Integer.parseInt(st.nextToken());
-        	if(sub==1){//가로로
-        		row.add(Integer.parseInt(st.nextToken()));
-        	}else{
-        		col.add(Integer.parseInt(st.nextToken()));
-        	}
-        }
-        
-        Collections.sort(row);
-        Collections.sort(col);
-        int r,c;
-        for(int i=0;i<row.size()-1;i++) {
-        	r=row.get(i+1)-row.get(i);
-        	for(int j=0;j<col.size()-1;j++) {
-        		c=col.get(j+1)-col.get(j);
-        		//System.out.println(r+" "+c);
-        		max=Math.max(max, r*c);
-        	}
-        }
-        
-        System.out.println(max);
-        
-    }
-}
+    	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+          StringTokenizer st=new StringTokenizer(br.readLine());
+          
+          int N=Integer.parseInt(st.nextToken());
+          int M=Integer.parseInt(st.nextToken());
+          int max=0;
+          int K=Integer.parseInt(br.readLine());
+          ArrayList<Integer> row=new ArrayList<>();
+          ArrayList<Integer> col=new ArrayList<>();
+          int sub;
+          row.add(0);
+          col.add(0);
+          row.add(N);
+          col.add(M);
+          
+          for(int i=0;i<K;i++) {
+          	st=new StringTokenizer(br.readLine());
+          	sub=Integer.parseInt(st.nextToken());
+          	if(sub==1){//가로로
+          		row.add(Integer.parseInt(st.nextToken()));
+          	}else{
+          		col.add(Integer.parseInt(st.nextToken()));
+          	}
+          }
+          
+          Collections.sort(row);
+          Collections.sort(col);
+          
+          int r=0;
+          int c=0;
+          
+          for(int i=0;i<row.size()-1;i++) {
+          	r=Math.max(r,row.get(i+1)-row.get(i));
+          }
+          
+          for(int i=0;i<col.size()-1;i++) {
+            	c=Math.max(c,col.get(i+1)-col.get(i));
+          }
+            
+          System.out.println(r*c);
+          
+      }
+  }
