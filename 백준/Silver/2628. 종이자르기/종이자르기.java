8d3152dc470099f3e3491909b8802306bc63a382
98SKC <127596280+98SKC@users.noreply.github.com>
2025-01-03 16:32:31 +0900

    [Silver V] Title: 종이자르기, Time: 72 ms, Memory: 11644 KB -BaekjoonHub

---
+++ 백준/Silver/2628. 종이자르기/종이자르기.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        
+        int N=Integer.parseInt(st.nextToken());
+        int M=Integer.parseInt(st.nextToken());
+        int max=0;
+        int K=Integer.parseInt(br.readLine());
+        ArrayList<Integer> row=new ArrayList<>();
+        ArrayList<Integer> col=new ArrayList<>();
+        int sub;
+        row.add(0);
+        col.add(0);
+        row.add(N);
+        col.add(M);
+        
+        for(int i=0;i<K;i++) {
+        	st=new StringTokenizer(br.readLine());
+        	sub=Integer.parseInt(st.nextToken());
+        	if(sub==1){//가로로
+        		row.add(Integer.parseInt(st.nextToken()));
+        	}else{
+        		col.add(Integer.parseInt(st.nextToken()));
+        	}
+        }
+        
+        Collections.sort(row);
+        Collections.sort(col);
+        int r,c;
+        for(int i=0;i<row.size()-1;i++) {
+        	r=row.get(i+1)-row.get(i);
+        	for(int j=0;j<col.size()-1;j++) {
+        		c=col.get(j+1)-col.get(j);
+        		//System.out.println(r+" "+c);
+        		max=Math.max(max, r*c);
+        	}
+        }
+        
+        System.out.println(max);
+        
+    }
+}
