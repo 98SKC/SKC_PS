6cefad5a6f5512573a2d7426b4ed8068787dcf99
98SKC <127596280+98SKC@users.noreply.github.com>
2025-01-11 13:08:14 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 72 ms, Memory: 11788 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -3,31 +3,39 @@ import java.io.*;
 
 public class Main {
 
-	//LIS를 dp로 푼 버전
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int N=Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        StringTokenizer st=new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
         
-        int[] arr=new int[N];
-        int[] dp= new int[N];
-        
-        for(int i=0;i<N;i++) {
-        	arr[i]=Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int max=0;
-        for(int i=0;i<N;i++) {
-        	dp[i]=1;
-        	for(int j=0;j<N;j++) {
-        		
-        		if(arr[i]<arr[j])dp[i]=Math.max(dp[i],dp[j]+1);
-        	}
-        	max=Math.max(dp[i], max);
+        
+        int length = 0; // LDS의 길이
+        
+        for (int i = 0; i < N; i++) {
+            int left = 0, right = length;
+            
+            // 이분 탐색으로 dp 배열에서 arr[i]가 삽입될 위치를 찾음
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (dp[mid] > arr[i]) { // 감소하는 방향
+                    left = mid + 1;
+                } else { // dp[mid] <= arr[i]
+                    right = mid;
+                }
+            }
+            
+            dp[left] = arr[i]; // 해당 위치를 갱신
+            if (left == length) length++; // 새로운 길이를 확장
+           // System.out.println(Arrays.toString(dp));
         }
-        System.out.println(max);
         
-        
+        System.out.println(length);
     }
 }
