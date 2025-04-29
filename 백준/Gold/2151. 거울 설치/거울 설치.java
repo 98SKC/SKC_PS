af92abd48c96cf6fc39fc79da8db6f4d6443ae8d
98SKC <127596280+98SKC@users.noreply.github.com>
2025-04-29 14:44:05 +0900

    [Gold III] Title: 거울 설치, Time: 104 ms, Memory: 13860 KB -BaekjoonHub

--- 백준/Gold/2151. 거울 설치/거울 설치.java
+++ 백준/Gold/2151. 거울 설치/거울 설치.java
@@ -1,13 +1,16 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+//다익스트라버전으로 두번째 풀이
+public class Main {
 
 	// 집 곳곳에 거울을 설치
 	// 집에 문이 두개. 한쪽 문에서 다른쪽 문을 볼수 있도록
 	// 이를 위한 최소의 거울 개수
 	// #은 문, .은 아무것도 없는 빛이 통과하는 곳. !거울 설치 가능. * 통과불가
 	// 빛이 직진을 하다가, 거울 설치 가능한 곳에서 전환
+    
+    
 	public static int[][][] v;
 	public static char[][] map;
 	public static int[] di=new int[] {0,1,0,-1};
@@ -42,8 +45,45 @@ public class Main{
 			}
 		}
 		int answer=Integer.MAX_VALUE;
+		PriorityQueue<int[]> pq=new PriorityQueue<>(new Comparator<int[]>() {
+			@Override
+			public int compare(int[]o1, int[] o2) {
+				return o1[3]-o2[3];
+			}
+		});
 		for(int a=0;a<4;a++) {
-			dfs(start/N,start%N,a,0);
+			v[start/N][start%N][a] = 0;
+			pq.add(new int[] {start/N,start%N,a,0});
+		}
+		int ni,nj,pi,pj;
+		int[] p;
+		
+		while(!pq.isEmpty()) {
+			p=pq.poll();
+			
+			if((p[0]*N+p[1])==exit) {
+				v[p[0]][p[1]][p[2]]=Math.min(v[p[0]][p[1]][p[2]], p[3]);
+				continue;
+			}
+			
+			if(map[p[0]][p[1]]=='!') {
+				for(int a=0;a<4;a++) {
+					if(a==p[2]) continue;
+					ni=p[0]+di[a];
+					nj=p[1]+dj[a];
+					if(ni>=0&&ni<N&&nj>=0&&nj<N&&map[ni][nj]!='*'&&v[ni][nj][a]>(p[3]+1)) {
+						v[ni][nj][a]=p[3]+1;
+						pq.add(new int[] {ni,nj,a,p[3]+1});
+					}
+				}
+			}
+			ni=p[0]+di[p[2]];
+			nj=p[1]+dj[p[2]];
+			if(ni>=0&&ni<N&&nj>=0&&nj<N&&map[ni][nj]!='*'&&v[ni][nj][p[2]]>p[3]) {
+				v[ni][nj][p[2]]=p[3];
+				pq.add(new int[] {ni,nj,p[2],p[3]});
+			}
+			
 		}
 		
 		for(int a=0;a<4;a++) {
@@ -53,37 +93,7 @@ public class Main{
 	
 	}
 	
-	public static void dfs(int pi, int pj, int dir,int count) {
-		if(v[pi][pj][dir]<=count) {
-			return;
-		}
-		v[pi][pj][dir]=count;
-		if((pi*N+pj)==exit) {
-			v[pi][pj][dir]=Math.min(v[pi][pj][dir], count);
-			return;
-		}
-		// #은 문, .은 아무것도 없는 빛이 통과하는 곳. !거울 설치 가능. * 통과불가
-		
-		//지금 위치가 거울 설치 가능, 빛 통과, 벽 세가지 경우
-		int ni,nj;
-		if(map[pi][pj]=='!'){
-			for(int a=0;a<4;a++) {
-				if(dir==a) continue;//같은 방향은 거울 설치 x
-				ni=pi+di[a];
-				nj=pj+dj[a];
-				if(ni>=0&&ni<N&&nj>=0&&nj<N&&map[ni][nj]!='*'&&v[ni][nj][a]>(count+1)) {					
-					dfs(ni,nj,a,count+1);
-				}
-			}		
-			//설치 가능해도 통과인 곳은 . 이랑 똑같이 처리
-		}
-		ni=pi+di[dir];
-		nj=pj+dj[dir];
-		if(ni>=0&&ni<N&&nj>=0&&nj<N&&map[ni][nj]!='*'&&v[ni][nj][dir]>count) {					
-			dfs(ni,nj,dir,count);
-		}
-		
-	}
+	
 	
 	
 
