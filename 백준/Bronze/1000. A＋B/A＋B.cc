85d476d00010a3ec9ee9367d8b5f74f5dd9a49fa
98SKC <127596280+98SKC@users.noreply.github.com>
2025-05-08 23:22:55 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    //cin << 입력넣을변수 입력
+    //cout << 출력할문장변수 출력
+    int N;
+    int M;
+
+    cin >> N;
+    cin >> M;
+
+    cout << N + M;
+
+    
+    return 0;                   // 정상 종료 시 0 반환
+}
