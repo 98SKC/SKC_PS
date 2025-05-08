05bec6d709ad4250f45c52564b50fb80eaf62d72
98SKC <127596280+98SKC@users.noreply.github.com>
2025-05-08 23:24:12 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    //cin << 입력넣을변수 입력
+    //cout << 출력할문장변수 출력
+    string str;
+
+    cin >> str;
+    
+
+    cout << str << "??!";
+
+    
+    return 0;                   // 정상 종료 시 0 반환
+}
