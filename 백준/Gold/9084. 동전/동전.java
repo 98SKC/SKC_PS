669b0920834e88a300a1199576d48e625f274243
98SKC <127596280+98SKC@users.noreply.github.com>
2025-05-16 23:31:23 +0900

    [Gold V] Title: 동전, Time: 68 ms, Memory: 11852 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.java
+++ 백준/Gold/9084. 동전/동전.java
@@ -1,95 +1,44 @@
+
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Main{
 
-	static int N,M;
-	static int[] money;
-	static int[] dp;
-	static int answer;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T=Integer.parseInt(br.readLine());
         StringBuilder sb=new StringBuilder();
-        
-        int T=Integer.parseInt(br.readLine());//3
         StringTokenizer st;
-        
-        for(int test_case=1;test_case<=T;test_case++) {
-        	answer=0;
-        	N=Integer.parseInt(br.readLine());//2
-        	
-        	money=new int[N];
-        	
+        int N,M;
+        int[] coin;
+        int[] dp;
+        for(int t=1;t<=T;t++) {
+        	N=Integer.parseInt(br.readLine());//동전의 가지 수
+        	coin=new int[N];
         	st=new StringTokenizer(br.readLine());
         	for(int i=0;i<N;i++) {
-        		money[i]=Integer.parseInt(st.nextToken());	
+        		coin[i]=Integer.parseInt(st.nextToken());
         	}
-        
+        	//N가지 동전으로 만들어야 할 금액 M
         	M=Integer.parseInt(br.readLine());
-       	dp=new int[M+1];
-        	
-//        	help2(0,M);
-        	
-        	for(int i=0;i<N;i++) {// money[i]원을 사용히여 만들 수 있는 돈의 개수
-        		for(int j=1;j<=M;j++) {//j원을 만들어야함.
-                    if(j-money[i]>0) {// 만약 j월보다 
-                        dp[j]+=dp[j-money[i]];
-                    }else if(j-money[i]==0) {
-                        dp[j]++;
-                    }
-                    
+        	dp=new int[M+1];
+        	dp[0]=1;
+        	for(int i=0;i<N;i++){
+        		for(int j=1;j<=M;j++) {
+        			if(j-coin[i]>=0) {
+        				//System.out.println(i+"원을 만드는 "+coin[j]+" 원 전의 개수: "+dp[i-coin[j]]);
+        				dp[j]+=dp[j-coin[i]];
+        			}
         		}
         	}
+        	//System.out.println(Arrays.toString(dp));
         	sb.append(dp[M]+"\n");
-//        	sb.append(answer+"\n");
+        	
         }
-  
         System.out.println(sb);
+        
+        
+        
     }
-
- /*   
-    static int help(int idx) {
-    	if(idx<0) {
-    		System.out.println("불가능한 경우입니다.");
-    		return 0;
-    	} 
-    	if(idx==0) {
-    		System.out.println("기본 단위입니다.");
-    		return 1;
-    	} 
-    	
-    	if(dp[idx]!=0) {
-    		System.out.println(idx+"원은 이미 구한 값입니다."+dp[idx]);
-    		return dp[idx];
-    	} 
-    	// dp[idx]-money[1]과 dp[idx]-money[2] 의 경로에 중첩이 있는 경우3=1+2 와 1+1+1로 나뉠 때 1+2의 2가 1+1인경우 중첩
-    	
-    	for(int i=0;i<N;i++) {
-    		System.out.println(idx+"를 구하기 위해"+(idx-money[i])+"를 구합니다.");
-    		dp[idx]+=help(idx-money[i]);//dp[idx-money[i]]에서 -money[i]를 더해서 오는 루트
-    	}
-    	System.out.println(idx+"를 구하였습니다: "+dp[idx]);
-    	System.out.println("----------------------------------------");
-    	
-    	return dp[idx];
-    }
-      */ 
-    //이건 시초날걸 예상했고
-//    static void help2(int pos,int won) {
-//    	if(pos==N-1) {
-//    		if(won%money[pos]==0) {
-//    			answer++;
-//    		}
-//    		return;
-//    	}
-//
-//    	int sub=won;
-//    	while(sub>=0) {
-//	
-//    		help2(pos+1,sub);
-//    		sub-=money[pos];//money[pos]원을 하나 늘리고, 다음 코인으로 넘어감.
-//    	}
-//    	
-//    	return;
-//    }
 }
