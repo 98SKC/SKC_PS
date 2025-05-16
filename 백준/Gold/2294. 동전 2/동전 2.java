88e4d782f63d8b5965fff849f529ea0bd593ee7d
98SKC <127596280+98SKC@users.noreply.github.com>
2025-05-17 00:05:29 +0900

    [Gold V] Title: 동전 2, Time: 80 ms, Memory: 11936 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,43 +1,34 @@
+
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
-        
         int N=Integer.parseInt(st.nextToken());
         int K=Integer.parseInt(st.nextToken());
-        
-        //int[] coin=new int[N];
-        ArrayList<Integer> coin=new ArrayList<>();
-        int[] dp=new int[100001]; 
-        dp[0]=0;
-        int sub; 
-        
-        for(int i=1;i<=K;i++) {
-        	dp[i]=K+1;
-        }
-        
+        int[] dp=new int[K+1];
+        int[] coin=new int[N];
         for(int i=0;i<N;i++) {
-        	//coin[i]=Integer.parseInt(br.readLine());
-        	sub=Integer.parseInt(br.readLine());
-        	coin.add(sub);
-        	dp[sub]=1;// 각 코인의 값은 그 코인 한개로 만들 수 있음
-        	//System.out.println(sub+" "+dp[sub]);
+        	coin[i]=Integer.parseInt(br.readLine());
+        }
+        int INF=10001;//1원으로 10000을 채우는 경우
+        Arrays.fill(dp, INF);
+        
+        dp[0] = 0;
+        
+        for(int c:coin) {
+        	for(int i=c;i<=K;i++) {
+        		dp[i]=Math.min(dp[i-c]+1, dp[i]);
+        	}
         }
         
-        	
-        for(int i=1;i<=K;i++) {
-        	for(Integer price:coin) {
-        		if(i-price>0) dp[i]=Math.min(dp[i-price]+1,dp[i]);
-        	}
-        	
-        }
-        //System.out.println(Arrays.toString(dp));
-        if(dp[K]==K+1) dp[K]=-1;
-        System.out.println(dp[K]);
+        if(dp[K]!=INF) System.out.println(dp[K]);
+        else System.out.println(-1);
+        
+        
         
     }
 }
