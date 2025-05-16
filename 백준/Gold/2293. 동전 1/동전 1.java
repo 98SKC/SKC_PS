2fa251a98022ec14da8c5afb9365dc234aab71ee
98SKC <127596280+98SKC@users.noreply.github.com>
2025-05-16 23:58:30 +0900

    [Gold IV] Title: 동전 1, Time: 76 ms, Memory: 11632 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,33 +1,28 @@
+
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
-        int n=Integer.parseInt(st.nextToken());
-        int k=Integer.parseInt(st.nextToken());
-        int[] coins=new int[n];
-        int[] dp= new int[k+1];
-        for(int i=0;i<n;i++) {
-        	coins[i]=Integer.parseInt(br.readLine());
-        	//dp[coins[i]]=1;
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int N=Integer.parseInt(st.nextToken());
+        int K=Integer.parseInt(st.nextToken());
+        int[] coin=new int[N];
+        int[] dp=new int[K+1];
+        for(int i=0;i<N;i++) {
+        	coin[i]=Integer.parseInt(br.readLine());
         }
-        
-        dp[0]=1;// 원래는 이게 없고 위의 주석과 아래의 if 조건이 i-coin>0 이였는데, 차이가 있다.
-        for(int coin:coins) {
-        	for(int i=1;i<=k;i++) {
-            	if(i-coin>=0) {
-            		dp[i]+=dp[i-coin];
-            	}
-            }
+        dp[0]=1;
+        for(int c:coin) {
+        	for(int k=c;k<=K;k++) {
+        		dp[k]+=dp[k-c];
+        	}
         }
+        System.out.println(dp[K]);
         
-
-        System.out.println(dp[k]);
         
-       
         
     }
 }
