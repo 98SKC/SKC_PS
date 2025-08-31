fbf0944bb9aa4c1c17719eb037c454f08aad7219
98SKC <127596280+98SKC@users.noreply.github.com>
2025-08-31 18:24:20 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 124 ms, Memory: 18372 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.java
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.java
@@ -49,34 +49,56 @@ public class Main {
         
     }
     
-    public static void findPalindrome(int index) {
-    	
-    	boolean find;
-    	int s,e;
-    	
-    	
-    	for(int start=1;start<=index;start++){
-    		for(int end=start;end<=index;end++) {
-    			find=true;
-    			s=start-1;
-    			e=end-1;
-				
-    			while (s <= e) {
-    				//앞과 뒤가 다르면? 펠리드롬이 아님.
-    				if (str.charAt(s++) != str.charAt(e--)) {
-						find = false;
-						break;
-					}
-				}
-				
-    			//위에서 false가 안되었다면 팰리드롬이 맞음
-				if(find) palindrome[start][end] = true;
-    			
-    		}
-    	}
-    	
-    	
-    }
-    
+//    public static void findPalindrome(int index) {
+//    	
+//    	boolean find;
+//    	int s,e;
+//    	
+//    	
+//    	for(int start=1;start<=index;start++){
+//    		for(int end=start;end<=index;end++) {
+//    			find=true;
+//    			s=start-1;
+//    			e=end-1;
+//				
+//    			while (s <= e) {
+//    				//앞과 뒤가 다르면? 펠리드롬이 아님.
+//    				if (str.charAt(s++) != str.charAt(e--)) {
+//						find = false;
+//						break;
+//					}
+//				}
+//				
+//    			//위에서 false가 안되었다면 팰리드롬이 맞음
+//				if(find) palindrome[start][end] = true;
+//    			
+//    		}
+//    	}
+//    	
+//    	
+//    }
+	
+	public static void findPalindrome(int n) {
+	    // 길이가 1인 구간은 항상 팰린드롬
+	    for (int i = 1; i <= n; i++) {
+	        palindrome[i][i] = true;
+	    }
+
+	    // 길이가 2 이상인 구간
+	    for (int len = 2; len <= n; len++) {
+	        for (int start = 1; start + len - 1 <= n; start++) {
+	            int end = start + len - 1;
+
+	            // 양 끝이 같으면서 가운데 구간이 팰린드롬이면 true
+	            if (str.charAt(start - 1) == str.charAt(end - 1)) {
+	                if (len == 2) { // 길이 2는 그냥 양 끝 같으면 됨
+	                    palindrome[start][end] = true;
+	                } else {
+	                    palindrome[start][end] = palindrome[start + 1][end - 1];
+	                }
+	            }
+	        }
+	    }
+	}
     
 }
