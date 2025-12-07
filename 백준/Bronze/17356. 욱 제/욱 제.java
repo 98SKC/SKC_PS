e4f61d21332cc0ff1b786bbd3d914df6691fd3e4
98SKC <127596280+98SKC@users.noreply.github.com>
2025-12-07 22:21:16 +0900

    [Bronze IV] Title: 욱 제, Time: 100 ms, Memory: 13100 KB -BaekjoonHub

---
+++ 백준/Bronze/17356. 욱 제/욱 제.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+        
+		double k = 1 / (1 + Math.pow(10, (b - a) / 400.0));
+		System.out.println(String.format("%.20f", k));
+	}
+}
