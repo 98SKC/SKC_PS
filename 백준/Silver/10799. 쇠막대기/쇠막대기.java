77c22eaddabda4ace4a2031d936e16fa484d7cec
98SKC <127596280+98SKC@users.noreply.github.com>
2026-03-13 22:17:11 +0900

    [Silver II] Title: 쇠막대기, Time: 88 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+ 
+        int open = 0;
+        int ans = 0;
+ 
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+ 
+            if (c == '(') { 
+                open++;
+            } else { 
+                open--; 
+ 
+                if (input.charAt(i - 1) == '(') { 
+                    ans += open; 
+                } else { 
+                    ans++; 
+                }
+            }
+        }
+ 
+        bw.write(ans + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+ 
+}
