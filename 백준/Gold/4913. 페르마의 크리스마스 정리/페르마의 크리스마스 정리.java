6165bdf9a9c435c94fa731b37db1615e250fe650
98SKC <127596280+98SKC@users.noreply.github.com>
2026-04-22 22:23:59 +0900

    [Gold IV] Title: 페르마의 크리스마스 정리, Time: 2860 ms, Memory: 16152 KB -BaekjoonHub

---
+++ 백준/Gold/4913. 페르마의 크리스마스 정리/페르마의 크리스마스 정리.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int INF = 1000000;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        boolean[] primeN = new boolean[INF];
+        primeN[0] = true;
+        primeN[1] = true;
+        for (int i = 2; i < INF; i++) {
+            if (!primeN[i]) {
+                for (int j = i + i; j < INF; j += i) {
+                    if (primeN[j]) continue;
+                    if (j % i == 0) primeN[j] = true;
+                }
+            }
+        }
+
+        
+        while (true) {
+            
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken());
+            int U = Integer.parseInt(st.nextToken());
+
+            if (L == -1 && U == -1) break;
+
+            
+            int tempL = L<0 ? 0 : L;
+            int tempU = U<0 ? 0 : U;
+            int x = 0;
+            int y = 0;
+
+
+            
+            
+            for (int i = tempL; i <= tempU; i++) {
+                if (!primeN[i]) {
+                    if (i % 4 == 1 || i == 2) y++;
+                    x++;
+                }
+            }
+
+            
+            
+            bw.write(L + " " + U + " " + x + " " + y);
+            bw.append("\n");
+            
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
