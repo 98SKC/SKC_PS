271360322e0c28f72d205a5622eb4c60e8f5a239
kyuchul5243 <kyuchul5243@gmail.com>
2026-05-05 23:24:05 +0900

    test : git account test

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        
+        int test=0; //깃 테스트를 위한 코드 변경점
         for (int i = 0; i < my_string.length(); i++) {
             char ch = my_string.charAt(i);
             
