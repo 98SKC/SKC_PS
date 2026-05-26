6411d31bd4362e0bcc7cd812cf187db8c215a39f
kyuchul5243 <kyuchul5243@gmail.com>
2026-05-26 23:35:05 +0900

    Predict Causes of Missing GitHub Contributions

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int n) {
         int[] answer=new int[n];
-        
+        int test=0; 
         int sub=0;
         
         for(int i=1;i<=n;i++){
