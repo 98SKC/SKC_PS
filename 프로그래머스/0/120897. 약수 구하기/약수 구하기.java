ffe6e702d6394e1768d57e942889247b0de8e8fd
98SKC <127596280+98SKC@users.noreply.github.com>
2026-05-26 23:28:53 +0900

    [level 0] Title: 약수 구하기, Time: 0.13 ms, Memory: 59.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer=new int[n];
+        
+        int sub=0;
+        
+        for(int i=1;i<=n;i++){
+            if(n%i==0) answer[sub++]=i;
+        }
+        
+        return Arrays.copyOf(answer,sub);
+    }
+}
